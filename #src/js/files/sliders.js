11a20bb413bd1f55158b27f09d206c436034a52c
grgp <grgp722@gmail.com>
2021-04-11 15:24:52 +0300

    main screen fixes

--- #src/js/files/sliders.js
+++ #src/js/files/sliders.js
@@ -1,9 +1,10 @@
 if($('.slider').length>0){
 	$('.slider').slick({
 		arrows: true,
+		infinite: true,
+		speed: 800,
 		autoplay: true,
-		speed: 500,
-		autoplaySpeed: 3000,
+		autoplaySpeed: 5000,
 
 		slidesToShow: 1,
 		slidesToScroll: 1,
